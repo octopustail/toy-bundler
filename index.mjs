25249156e0c04f8d8fb3e71a59eaa85a63fc94c5
valeywzhang <valeywzhang@tencent.com>
2023-06-06 17:04:06 +0800

    feat: ✅ resolve dependencies graph

--- index.mjs
+++ index.mjs
@@ -5,13 +5,15 @@ import { fileURLToPath } from "url";
 import { resolve } from "path";
 import chalk from "chalk";
 import yargs from "yargs";
-import { ChildProcess } from "child_process";
+import Resolver from "jest-resolve";
+import { DependencyResolver } from "jest-resolve-dependencies";
 
 const options = yargs(process.argv).argv;
 const entryPoint = resolve(process.cwd(), options.entryPoint);
 
-
 const root = join(dirname(fileURLToPath(import.meta.url)), 'product');
+
+
 
 const hasteMapOptions = {
     extensions: ['js'],
@@ -26,13 +28,43 @@ const hasteMap = new JestHasteMap.default(hasteMapOptions);
 
 await hasteMap.setupCachePath(hasteMapOptions);
 
-const { hasteFS, moudleMap } = await hasteMap.build();
+const { hasteFS, moduleMap } = await hasteMap.build();
 
-if(!hasteFS.exists(entryPoint)){
+if (!hasteFS.exists(entryPoint)) {
     throw new Error(
         '`--entry-point` dose not exist. Please provide a path to a valid file'
     )
 }
+const resolver = new Resolver.default(moduleMap, {
+    extensions: ['.js'],
+    hasCoreModules: false,
+    rootDir: root,
+})
 
-console.log(hasteFS.getAllFiles());
-console.log(chalk.bold(`❯ Building ${chalk.blue(options.entryPoint)}`))
+const dependencyResolver = new DependencyResolver(resolver, hasteFS);
+
+
+
+console.log(dependencyResolver.resolve(entryPoint));
+
+
+const allFiles = new Set();
+const queue = [entryPoint];
+
+while(queue.length){
+    const module = queue.shift();
+    
+    // guard for cycles; 
+    if(allFiles.has(module)){
+        // skip resolved dependencies rather than throw an error;
+        continue;
+    }
+    
+    allFiles.add(module);
+    queue.push(...dependencyResolver.resolve(module));
+}
+
+
+console.log(chalk.bold(`❯ Building ${chalk.blue(entryPoint)}`))
+console.log(chalk.bold(`❯ Found ${chalk.blue(allFiles.size)} files`));
+console.log(Array.from(allFiles));
